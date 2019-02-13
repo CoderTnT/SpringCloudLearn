0682046d53201562436ae478576c22144fb0342a
liaoshaojie <liaoshaojie@ilinklink.com>
2019-02-13 11:30:56 +0800

    eureka服务注册中心与服务提供者

---
+++ springcloud2/eureka-client/src/main/java/com/cooper/eurekaclient/HiController.java
@@ -0,0 +1,24 @@
+package com.cooper.eurekaclient;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author liaoshaojie
+ * @creatdate 2019/2/13
+ * @describe
+ */
+@RestController
+public class HiController {
+
+    @Value("${server.port}")
+    String port;
+
+    @GetMapping("/hi")
+    public String sayHi(@RequestParam String name) {
+
+        return "Hi i'm "+ name +" ,and i come from port" + port;
+    }
+}
