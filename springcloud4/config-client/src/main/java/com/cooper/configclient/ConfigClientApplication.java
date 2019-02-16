631c3915d0c42947b5e97fdc5af5deff85fb2811
liaoshaojie <liaoshaojie@ilinklink.com>
2019-02-16 10:06:19 +0800

    配置中心高可用，以及读取本地或者远程git配置，远程配置可通过bus自动刷新多个服务实例

---
+++ springcloud4/config-client/src/main/java/com/cooper/configclient/ConfigClientApplication.java
@@ -0,0 +1,31 @@
+package com.cooper.configclient;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+@EnableEurekaClient
+@RefreshScope
+public class ConfigClientApplication {
+
+    @Value("${foo}")
+    String foo;
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientApplication.class, args);
+    }
+
+    @GetMapping("/foo")
+    public String foo() {
+        return foo;
+    }
+
+}
+
+
